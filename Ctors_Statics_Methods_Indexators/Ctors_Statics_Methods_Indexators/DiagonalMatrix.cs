9caa60f6175f14c05e3f143e47831611c152331f
Anton Vlasik <antonvlasik@coherentsolutions.com>
2018-10-08 16:42:30 +0300

    after review

---
+++ Ctors_Statics_Methods_Indexators/Ctors_Statics_Methods_Indexators/DiagonalMatrix.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+namespace Ctors_Statics_Methods_Indexators
+{
+    public class DiagonalMatrix
+    {
+        private int[] _mainDiagonal;
+
+        public int Size { get; }
+
+        public int this[int i, int j]
+        {
+            get => CheckIndex(i) && CheckIndex(j) && (i == j) ? _mainDiagonal[i] : 0;
+
+            set
+            {
+                if (CheckIndex(i) && CheckIndex(j) && (i == j))
+                {
+                    _mainDiagonal[i] = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    result.Append($"{this[i, j]} ");
+                }
+                result.AppendLine();
+            }
+            return result.ToString();
+        }
+
+        public DiagonalMatrix(params int[] mainDiagonalElements)
+        {
+            if (mainDiagonalElements == null || mainDiagonalElements.Length == 0)
+            {
+                Size = 0;
+                _mainDiagonal = new int[Size];
+            }
+            else
+            {
+                Size = mainDiagonalElements.Length;
+                _mainDiagonal = new int[Size];
+                for (int i = 0; i < Size; i++)
+                {
+                    _mainDiagonal[i] = mainDiagonalElements[i];
+                }
+            }
+        }
+
+        public int Track()
+        {
+            int sum = 0;
+            foreach (var element in _mainDiagonal)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+
+        public DiagonalMatrix Add(DiagonalMatrix addedMatrix)
+        {
+            int newMatrixLength = Math.Max(addedMatrix.Size, this.Size);
+            int[] newMainDiagonal = new int[newMatrixLength];
+            for (int i = 0; i < newMatrixLength; i++)
+            {
+                newMainDiagonal[i] = this[i, i] + addedMatrix[i, i];
+            }
+
+            return new DiagonalMatrix(newMainDiagonal);
+        }
+
+        public static DiagonalMatrix Identity(int n)
+        {
+            if (n < 0)
+            {
+                return new DiagonalMatrix(new int[0]);
+            } else
+            {
+                int[] mainDiagonal = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    mainDiagonal[i] = 1;
+                }
+                return new DiagonalMatrix(mainDiagonal);
+            }
+        }
+
+        private bool CheckIndex(int i) => i >= 0 && i < Size;
+    }
+}
--- Ctors_Statics_Methods_Indexators/Ctors_Statics_Methods_Indexators/Program.cs
+++ Ctors_Statics_Methods_Indexators/Ctors_Statics_Methods_Indexators/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ctors_Statics_Methods_Indexators
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -49,110 +45,6 @@ namespace Ctors_Statics_Methods_Indexators
             Console.WriteLine("Your Identity Matrix: ");
             Console.WriteLine(DiagonalMatrix.Identity(identitySize));
             Console.ReadLine();
-        }
-    }
-
-    class DiagonalMatrix
-    {
-        private int[] mainDiagonal;
-        public int Size {get;}
-
-        public override string ToString()
-        {
-            string result = "";
-            for (int i = 0; i < Size; i++) 
-            {
-                for(int j = 0; j < Size; j++)
-                {
-                    result += $"{this[i, j]} ";
-                }
-                result += "\n";
-            }
-            return result;
-        }
-
-        public DiagonalMatrix(params int[] mainDiagonalElements)
-        {
-            if (mainDiagonalElements == null)
-            {
-                Size = 0;
-                mainDiagonal = new int[Size];
-            }
-            else
-            {
-                Size = mainDiagonalElements.Length;
-                mainDiagonal = new int[Size];
-                for (int i = 0; i < Size; i++)
-                {
-                    mainDiagonal[i] = mainDiagonalElements[i];
-                }
-            }
-
-        }
-
-        public int this[int i, int j]
-        {
-            get
-            {
-                if(i >= Size || j >= Size)
-                {
-                    return 0;
-                }
-
-                if (i != j)
-                {
-                    return 0;
-                }
-                else
-                {
-                    return mainDiagonal[i];
-                }
-            }
-
-            set
-            {
-                if ((i < Size || j < Size) && i == j)
-                {
-                    mainDiagonal[i] = value;
-                }
-            }
-        }
-
-        public int this[int i]
-        {
-            get => i >= Size || i < 0 ? 0 : mainDiagonal[i];
-        }
-
-        public int Track()
-        {
-            int sum = 0;
-            foreach (var element in mainDiagonal)
-            {
-                sum += element;
-            }
-            return sum;
-        }
-
-        public DiagonalMatrix Add(DiagonalMatrix addedMatrix)
-        {
-            int newMatrixLength = Math.Max(addedMatrix.Size, this.Size);
-            int[] newMainDiagonal = new int[newMatrixLength];
-            for (int i = 0; i < newMatrixLength; i++)
-            {
-                newMainDiagonal[i] = this[i] + addedMatrix[i];
-            }
-
-            return new DiagonalMatrix(newMainDiagonal);
-        }
-
-        public static DiagonalMatrix Identity(int n)
-        {
-            int[] mainDiagonal = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                mainDiagonal[i] = 1;
-            }
-            return new DiagonalMatrix(mainDiagonal);
         }
     }
 }
