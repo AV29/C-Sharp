f6958d4747c6d2418adf3b9be446642cca532266
Anton Vlasik <snumber29@gmail.com>
2018-12-11 23:42:43 +0300

    Added Collections and LinQ example

---
+++ dotNet_2/Taks_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Taks_1
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var vacations = GenerateVacations(30, 2017, 2018);
+
+            foreach (var item in vacations)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<Vacation> GenerateVacations(int count, int startYear, int endYear) {
+            string[] names = new string[] {"John", "Peter", "Michael", "Anna", "Sara", "David", "Frank", "Alfred"};
+            List<Vacation> vacations = new List<Vacation>(count);
+            Random random = new Random();
+            for (int i = 0; i < count; i ++) {
+                DateTime startDate = new DateTime(startYear, random.Next(1, 13), random.Next(1, 30));
+                DateTime endDate = new DateTime(endYear, random.Next(1, 13), random.Next(1, 30));
+                vacations.Add(new Vacation(startDate, endDate, names[random.Next(1, names.Length)]));
+            }
+            return vacations;
+        }
+    }
+}
---
+++ dotNet_2/Taks_1/Vacation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Taks_1
+{
+    public class Vacation
+    {
+        #region Properties
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Vacation(DateTime startDate, DateTime endDate, string employeeName)
+        {
+            EmployeeName = employeeName;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return $"{EmployeeName} - {StartDate} -> {EndDate}";
+        }
+    }
+}
