f8312aa39fcb745ae85a38bce017fc0beb11c44e
Anton Vlasik <snumber29@gmail.com>
2018-12-12 00:49:13 +0300

    added average calculations

--- dotNet_2/Taks_1/Program.cs
+++ dotNet_2/Taks_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 namespace Taks_1
 {
@@ -6,22 +7,39 @@ namespace Taks_1
     {
         public static void Main(string[] args)
         {
-            var vacations = GenerateVacations(30, 2017, 2018);
+            var vacations = GenerateVacations(50, 2018);
 
+            var averageVacationInfo = vacations
+                .GroupBy(v => v.name)
+                .Select(group => new { Name = group.Key, Average = (int)group.Average(v => (v.end - v.start).Days) });
+
+            var overallAverageVacationLength = (int)averageVacationInfo.Average(info => info.Average);
+
+            Console.WriteLine("----------------All-----------------");
             foreach (var item in vacations)
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine();
+            Console.WriteLine("------------ Average Vacation Info ----------------");
+            foreach (var item in averageVacationInfo)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Overall average vacation length = {overallAverageVacationLength}");
         }
 
-        public static List<Vacation> GenerateVacations(int count, int startYear, int endYear) {
-            string[] names = new string[] {"John", "Peter", "Michael", "Anna", "Sara", "David", "Frank", "Alfred"};
-            List<Vacation> vacations = new List<Vacation>(count);
+        public static List<(DateTime start, DateTime end, string name)> GenerateVacations(int count, int year)
+        {
+            string[] names = new string[] { "John", "Peter", "Michael", "Anna", "Sara", "David", "Frank", "Alfred" };
+            var vacations = new List<(DateTime, DateTime, string)>(count);
             Random random = new Random();
-            for (int i = 0; i < count; i ++) {
-                DateTime startDate = new DateTime(startYear, random.Next(1, 13), random.Next(1, 30));
-                DateTime endDate = new DateTime(endYear, random.Next(1, 13), random.Next(1, 30));
-                vacations.Add(new Vacation(startDate, endDate, names[random.Next(1, names.Length)]));
+            for (int i = 0; i < count; i++)
+            {
+                DateTime startDate = new DateTime(year, random.Next(5, 7), random.Next(1, 30));
+                DateTime endDate = new DateTime(year, random.Next(7, 9), random.Next(1, 30));
+                vacations.Add((startDate, endDate, names[random.Next(1, names.Length)]));
             }
             return vacations;
         }
